fdbc8c0aef54d7ea01559e40d47bce7d4f3278ca
SarathLUN <lun.sarath@gmail.com>
2023-12-06 00:10:38 +0700

    add markdown preview

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -72,4 +72,13 @@ lvim.plugins = {
   -- "MunifTanjim/nui.nvim",
   -- "jackMort/ChatGPT.nvim",
   -- "Bryley/neoai.nvim"
+  {
+  "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
